cae6f0b7d97459ba46ef6af24689dff3c822611b
Andriiv Volodimirovich <deen70456@gmail.com>
2023-03-22 22:15:45 +0200

    add maket

--- MyCRM/Startup.cs
+++ MyCRM/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -8,32 +9,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyCRM
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+namespace MyCRM{
+    public class Startup{
+
+        //public IConfiguration Configuration { get; }
+        //public Startup(IHostEnvironment hostEnv){
+        //    Configuration = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("DBSettings.json").Build(); ;
+        //}
+
+        public void ConfigureServices(IServiceCollection services){
+            // services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddTransient<IallCategory, CategoryReposetory>();
+            //services.AddTransient<IallGameItem, GameItemReposetory>();
+            //services.AddTransient<IallComments, CommentsReposetory>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddScoped(sp =>)
+
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+
+            services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env){
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseStaticFiles();
+            app.UseSession();
+            //{ ID ?}
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("defult", "{controller=Home}/{action=Home}/{id?}");
             });
         }
     }
